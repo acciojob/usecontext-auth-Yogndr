3ab325b0a54f3f17389da3b7b5bf01cb58a65f49
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-09-06 11:23:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 
-import React from "react";
+import React, { createContext, useState } from "react";
 import './../styles/App.css';
+import Auth from "./Auth";
+
+
+ export const authContext=createContext();
+ 
 
 const App = () => {
+  const[authentication,setAuthentication]=useState(false);
+ 
   return (
+    <authContext.Provider value={{authentication,setAuthentication}}>
     <div>
-        {/* Do not remove the main div */}
+        <h1>Click on the checkbox to get authenticated</h1>
+        
+        <Auth/>
     </div>
+    </authContext.Provider>
   )
 }
 
