8d602d59153ffe617c946c180bbcffc1c0c47c02
Yogender <yashmittal92002@gmail.com>
2025-09-06 11:19:31 +0530

    updated

---
+++ src/components/Auth.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import { authContext } from './App'
+const Auth = () => {
+    const {authentication,setAuthentication}=useContext(authContext)
+
+  return (
+    <div>
+        {authentication?(
+            <p>You are now authenticated, you can proceed</p>
+        ):(
+            <p>You are not authenticated</p>
+        )}
+
+        <input type="checkbox" checked={authentication} onChange={()=>{
+            setAuthentication(!authentication)
+        }}/>
+        I'm not a robot
+    </div>
+  )
+}
+
+export default Auth
